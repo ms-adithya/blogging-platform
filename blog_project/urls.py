229de9286deda757cc3b7e716f95d60535ba85c2
Adithya M S <65106852+Empuraan015x@users.noreply.github.com>
2025-03-06 22:39:25 +0530

    MIME fix: MIME type ('text/html') is not executable

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -22,3 +22,9 @@ urlpatterns = [
     path('api/', include('blogs.urls')),  # Include API URLs
     path('api/auth/', include('authentication.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
